0e8cea6a11791adf0588712ee77a859ad7d7c842
laverabl <podoprygina@MacBook-Air-Valeria.local>
2023-08-23 15:04:02 +0300

    finished

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,9 +3,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
 
 public class Main {
     private static UserService userService = new UserServiceImpl();
